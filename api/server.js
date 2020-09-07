0f9810b9dd828effd14ed3aa3cc36720d0ba43f9
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-07 15:28:01 -0400

    working on CRUD functionality usingknex

--- api/server.js
+++ api/server.js
@@ -6,4 +6,33 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get('/', (req, res) => {
+    db.select('*').from('accounts')
+        .then(accounts => {
+            res.status(200).json({ data: accounts })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ err: err.message })
+        })
+})
+
+server.get('/:id', (req, res) => {
+    db.select()
+})
+
+server.post('/', (req, res) => {
+    const post = req.body;
+
+    db('accounts').insert(post)
+        .then(ids => {
+            res.status(201).json({ inserted: ids })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ err: err.message })
+        })
+})
+
 module.exports = server;
