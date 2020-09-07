4be734a9edd26383275a6dd42d0e200f7e006295
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-07 15:44:19 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -18,16 +18,51 @@ server.get('/', (req, res) => {
         })
 })
 
-server.get('/:id', (req, res) => {
-    db.select()
-})
+// server.get('/:id', (req, res) => {
+//     db.select()
+// })
 
 server.post('/', (req, res) => {
     const post = req.body;
 
-    db('accounts').insert(post)
+    db('accounts').insert(post, 'id')
         .then(ids => {
             res.status(201).json({ inserted: ids })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ err: err.message })
+        })
+})
+
+server.put('/:id', (req, res) => {
+    const changes = req.body
+    const id = req.params.id
+
+    db('accounts').where({ id: id }).update(changes)
+        .then(count => {
+            if(count) {
+                res.status(200).json({ messgae: "updated successfully" })
+            } else { 
+                res.status(404).json({ error: "thing not found" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ err: err.message })
+        })
+})
+
+server.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    db('accounts').where({ id: id }).del()
+        .then(account => {
+            if(account) {
+                res.status().json({ message: "successfully deleted account" })
+            } else {
+                res.status(404).json({ error: "account does not exist" })
+            }
         })
         .catch(err => {
             console.log(err)
